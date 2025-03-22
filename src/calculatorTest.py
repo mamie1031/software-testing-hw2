52bb5ce6d2db2b053c4f1eec11bb3a742fa1d100
mamie1031 <sciencemamie@gmail.com>
2025-03-22 11:22:13 +0800

    test: add substract test

---
+++ src/calculatorTest.py
@@ -0,0 +1,16 @@
+import unittest
+from calculator import Calculator  # The class we are going to implement
+
+class TestCalculator(unittest.TestCase):
+    def test_add(self):
+        calc = Calculator()
+        result = calc.add(2, 3)
+        self.assertEqual(result, 5)  # Expect 2 + 3 = 5
+        
+    def test_substract(self):
+        calc = Calculator()
+        result = calc.substract(2, 3)
+        self.assertEqual(result, -1)
+
+if __name__ == "__main__":
+    unittest.main()
