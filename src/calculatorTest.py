27d0702d81cd5cce904ed373bcd58fd417d7c015
mamie1031 <sciencemamie@gmail.com>
2025-03-22 11:27:52 +0800

    test: add multiply test

--- src/calculatorTest.py
+++ src/calculatorTest.py
@@ -6,11 +6,16 @@ class TestCalculator(unittest.TestCase):
         calc = Calculator()
         result = calc.add(2, 3)
         self.assertEqual(result, 5)  # Expect 2 + 3 = 5
-        
+
     def test_substract(self):
         calc = Calculator()
         result = calc.substract(2, 3)
         self.assertEqual(result, -1)
+
+    def test_multiply(self):
+        calc = Calculator()
+        result = calc.multiply(2, 3)
+        self.assertEqual(result, 6)
 
 if __name__ == "__main__":
     unittest.main()
